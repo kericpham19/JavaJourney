d5e3fc232b13571682c8b2bced87c68b1bb30f07
kericpham19 <131621286+kericpham19@users.noreply.github.com>
2024-08-02 19:53:56 -0400

    Add files via upload
    HelloWorld code!

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+public class HelloWorld
+{
+   public static void main(String []args)
+   {
+      JOptionPane.showMessageDialog(null, "Hello World! \nHow are you?", "Title", JOptionPane.WARNING_MESSAGE);
+   }
+}
